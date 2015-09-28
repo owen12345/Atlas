6e9b4041269c85a4a2e4607e18138de5f7969ba1
System Administrator <root@OwenWiesMacBook.home>
2015-09-28 15:08:33 -0400

    added rotation on x and y.

---
+++ Assets/Scripts/EarthBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthBehavior : MonoBehaviour {
+
+	private float _sensitivity;
+	private Vector3 _mouseReference;
+	private Vector3 _mouseOffset;
+	private Vector3 _rotation;
+	private bool _isRotating;
+
+	// Use this for initialization
+	void Start () {
+		_sensitivity = 0.4f;
+		_rotation = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_isRotating)
+		{
+			// offset
+			_mouseOffset = (Input.mousePosition - _mouseReference);
+			
+			// apply rotation
+			_rotation.y = -(_mouseOffset.x) * _sensitivity;
+			_rotation.x = (_mouseOffset.y) * _sensitivity;
+			
+			// rotate
+			transform.Rotate(_rotation,Space.World);
+			
+			// store mouse
+			_mouseReference = Input.mousePosition;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		// rotating flag
+		_isRotating = true;
+		
+		// store mouse
+		_mouseReference = Input.mousePosition;
+	}
+
+	void OnMouseUp()
+	{
+		// rotating flag
+		_isRotating = false;
+	}
+}
+
