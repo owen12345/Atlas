04f51fca34f96d0ed29110cc5cba711c53732809
System Administrator <root@OwenWiesMacBook.home>
2015-10-22 14:50:25 -0400

    updates earth rotation abilities

---
+++ Assets/Scripts/DragAndRotateEarth.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine; 
+using System.Collections;  
+
+
+public class DragAndRotateEarth : MonoBehaviour { 
+	
+	
+	bool hasGrabbedPoint = false;
+	Vector3 grabbedPoint;
+
+	void Start () {
+
+	}
+
+	void Update () { //This is not running
+		for (float i = 0; i < 100; i ++) {
+			print (i);
+		}   
+		 
+
+		if (Input.GetMouseButton (0)) { 
+			if (!hasGrabbedPoint) { 
+				hasGrabbedPoint = true; 
+				grabbedPoint = getTouchedPoint (); 
+			} else { 
+				Vector3 targetPoint = getTouchedPoint (); 
+				Quaternion rot = Quaternion.FromToRotation (grabbedPoint, targetPoint); 
+				transform.rotation *= rot;
+			} 
+		} else { 
+			hasGrabbedPoint = false; 
+		}
+	}
+	
+	
+	Vector3 getTouchedPoint() { 
+		RaycastHit hit; 
+
+		Physics.Raycast (Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+		
+		
+		return transform.InverseTransformPoint(hit.point);
+		
+	} } 
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (float i = 0; i < 100; i ++) {
+			print(i);
+		}
+	}
+}
